d9c1551e3fd3a53fbf3d975ba5aa86a096c633db
Livesteelfighter <KC0FMQ@YAHOO.COM>
2023-06-14 14:30:53 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
 function alertButton(){
  alert("Hello World!") 
 }
+
+function Weather() { window.open ("https://api.openweathermap.org/data/2.5/weather?zip=34684&appid=2cb44e273a38eb19888d8aeec151da02&units=imperial&mode=html);
+                                  }
