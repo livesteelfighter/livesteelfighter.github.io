581a98651341d26cd5eb85fdbbc8c4ad095e8f99
Livesteelfighter <KC0FMQ@YAHOO.COM>
2023-06-15 14:24:14 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 function alertButton(){
  alert("Hello World!") 
 }
-function Weather(){ fetch('https://api.openweathermap.org/data/2.5/weather?zip=34684&appid=2cb44e273a38eb19888d8aeec151da02&units=imperial&mode=html')
+function Weather(){ fetch('https://api.openweathermap.org/data/2.5/weather?zip=34684&appid=2cb44e273a38eb19888d8aeec151da02&units=imperial&mode=html', 
+                          {headers: {'Accept' : "application/json'}})
+                   
                    .then(response => response.text())
                    .then(text => console.log(text))
                   }
